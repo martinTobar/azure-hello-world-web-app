c6d028b2b4488adf6c2b158d161f45942b46bfc4
martinTobar <martoflo99@gmail.com>
2025-03-28 00:02:20 -0500

    playing around

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! my name is Martin Az'
+    return 'Hello, World! my name is Martin from Azure'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
