a444ea6c77acca00c55480cefa0cd23024f86754
martinTobar <martoflo99@gmail.com>
2025-03-28 15:31:22 -0500

    more playing

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! my name is Martin from Azure'
+    keyVaultName = 'myKeyVaultForLearning'
+    KVUri = f"https://{keyVaultName}.vault.azure.net"
 
+    credential = DefaultAzureCredential()
+    client = SecretClient(vault_url=KVUri, credential=credential)
+
+    retrieved_secret = client.get_secret('mySecret')
+    return f'Hello, World! my name is Martin from Azure secret is {retrieved_secret.value}'
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
