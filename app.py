6534c1017342ffc3239222f23cd59556a28b1e66
martinTobar <martoflo99@gmail.com>
2025-04-02 23:25:25 -0500

    addded logger and better credentials handling

--- app.py
+++ app.py
@@ -5,13 +5,31 @@ from dotenv import load_dotenv
 import pyodbc, struct
 from azure import identity
 import os
-
+import logging
 app = Flask(__name__)
 
 connection_string = os.getenv("AZURE_SQL_CONNECTIONSTRING")
+# Set the logging level for the Azure Identity library
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler()
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+#logging.basicConfig(level=logging.INFO)
+logger.info('Started')
+logger.error("Starting with error debug dont get scared")
+# Direct logging output to stdout. Without adding a handler,
+# no logging output is visible.
 
 def get_conn():
-    credential = identity.DefaultAzureCredential(exclude_interactive_browser_credential=True)
+    logger.info('\n==========================================\n')
+    if "WEBSITE_INSTANCE_ID" in os.environ:
+        logger.info("Running in Azure App Service")
+        # DefaultAzureCredential will automatically use the Managed Identity in Azure App Service
+        credential = DefaultAzureCredential()
+    else:
+        logger.info("Running locally")
+        credential =  identity.DefaultAzureCredential(exclude_interactive_browser_credential=True)
+    logger.info('\n==========================================\n')   
     token_bytes = credential.get_token("https://database.windows.net/.default").token.encode("UTF-16-LE")
     token_struct = struct.pack(f'<I{len(token_bytes)}s', len(token_bytes), token_bytes)
     SQL_COPT_SS_ACCESS_TOKEN = 1256  # This connection option is defined by microsoft in msodbcsql.h
